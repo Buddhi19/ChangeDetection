add53a25c2084f7e61b7a503e41b0df5d7778877
Buddhi19 <buddhiwijenayake34802@gmail.com>
2024-09-09 22:14:41 +0530

    first commit

---
+++ CDlib/Dataloader.py
@@ -0,0 +1,80 @@
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import os
+import numpy as np
+import sys
+from PIL import Image
+from skimage import io
+
+main_path = os.path.abspath(os.path.dirname((os.path.dirname(os.path.dirname(__file__)))))
+sys.path.append(main_path)
+
+SECOND_DATASET_PATH = os.path.join(main_path, 'MAMBA/SECOND/SECOND')
+
+TRAIN_PATH_T1 = os.path.join(SECOND_DATASET_PATH, 'train/T1')
+TRAIN_PATH_T2 = os.path.join(SECOND_DATASET_PATH, 'train/T2')
+LABEL_PATH = os.path.join(SECOND_DATASET_PATH, 'train/GT_CD')
+
+FILE_NAMES_T1 = os.path.join(SECOND_DATASET_PATH, 'T1.txt')
+FILE_NAMES_T2 = os.path.join(SECOND_DATASET_PATH, 'T2.txt')
+FILE_NAMES_LABEL = os.path.join(SECOND_DATASET_PATH, 'GT_CD.txt')
+
+class BCD_DATALOADER(Dataset):
+    def __init__(
+            self, train_path_T1 = TRAIN_PATH_T1, train_path_T2 = TRAIN_PATH_T2,
+            label_path = LABEL_PATH, file_names_T1 = FILE_NAMES_T1,
+            file_names_T2 = FILE_NAMES_T2, file_names_label = FILE_NAMES_LABEL,
+            transform = None, transform_label = None
+        ):
+        self.train_path_T1 = train_path_T1
+        self.train_path_T2 = train_path_T2
+        self.label_path = label_path
+        self.transform = transform
+        self.transform_label = transform_label
+
+        self.file_names_T1 = self.read_file_names(file_names_T1)
+        self.file_names_T2 = self.read_file_names(file_names_T2)
+        self.file_names_label = self.read_file_names(file_names_label)
+
+        self.common_files = list(set(self.file_names_T1) & set(self.file_names_T2) & set(self.file_names_label))
+
+    def read_file_names(self, file_names):
+        with open(file_names, 'r') as f:
+            return f.read().splitlines()
+        
+    def __len__(self):
+        return len(self.common_files)
+    
+    def __getitem__(self, idx):
+        img_name_T1 = os.path.join(self.train_path_T1, self.common_files[idx])
+        img_name_T2 = os.path.join(self.train_path_T2, self.common_files[idx])
+        label_name = os.path.join(self.label_path, self.common_files[idx])
+
+        image_T1 = io.imread(img_name_T1)
+        image_T2 = io.imread(img_name_T2)
+        label = io.imread(label_name)
+
+        image_T1 = Image.open(img_name_T1).convert('RGB')
+        image_T2 = Image.open(img_name_T2).convert('RGB')
+        label = Image.open(label_name).convert('L')
+
+        if self.transform:
+            image_T1 = self.transform(image_T1)
+            image_T2 = self.transform(image_T2)
+            label = self.transform_label(label)
+
+        return image_T1, image_T2, label
+    
+
+###########################################################
+    
+if __name__ == '__main__':
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+    dataset = BCD_DATALOADER(transform=transform)
+    print(len(dataset))
+    print(dataset[0])
---
+++ CDlib/List_out.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+main_path = os.path.abspath(os.path.dirname((os.path.dirname(os.path.dirname(__file__)))))
+sys.path.append(main_path)
+
+SECOND_DATASET_PATH = os.path.join(main_path, 'MAMBA/SECOND/SECOND')
+TRAIN_PATH_T1 = os.path.join(SECOND_DATASET_PATH, 'train/T1')
+TRAIN_PATH_T2 = os.path.join(SECOND_DATASET_PATH, 'train/T2')
+LABEL_PATH = os.path.join(SECOND_DATASET_PATH, 'train/GT_CD')
+
+def list_out():
+    files_T1 = []
+    for file in os.listdir(TRAIN_PATH_T1):
+        files_T1.append(file)
+    files_T2 = []
+    for file in os.listdir(TRAIN_PATH_T2):
+        files_T2.append(file)
+    files_LABEL = []
+    for file in os.listdir(LABEL_PATH):
+        files_LABEL.append(file)
+
+    T1_OUT = os.path.join(SECOND_DATASET_PATH, 'T1.txt')
+    T2_OUT = os.path.join(SECOND_DATASET_PATH, 'T2.txt')
+    LABEL_OUT = os.path.join(SECOND_DATASET_PATH, 'GT_CD.txt')
+
+    with open(T1_OUT, 'w+') as f:
+        for item in files_T1:
+            f.write("%s\n" % item)
+
+    with open(T2_OUT, 'w+') as f:
+        for item in files_T2:
+            f.write("%s\n" % item)
+
+    with open(LABEL_OUT, 'w+') as f:
+        for item in files_LABEL:
+            f.write("%s\n" % item)
+
+if __name__ == '__main__':
+    list_out()
