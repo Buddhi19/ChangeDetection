e9de73fea5afa23d0bd6e170eebe4d98802688cd
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-09-13 13:34:48 +0530

    Update SYSU dataset preparation and list output

--- CDlib/List_out.py
+++ CDlib/List_out.py
@@ -1,181 +1,163 @@
 import sys
 import os
 import shutil
+from tqdm import tqdm
 
 main_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(main_path)
 
-SECOND_DATASET_PATH = os.path.join(main_path, 'Datasets')
+DATASET_PATH = os.path.join(main_path, 'Datasets')
 
-TRAIN_PATH_T1 = os.path.join(SECOND_DATASET_PATH,'SECOND' ,'train/T1')
-TRAIN_PATH_T2 = os.path.join(SECOND_DATASET_PATH,'SECOND' ,'train/T2')
-LABEL_PATH = os.path.join(SECOND_DATASET_PATH,'SECOND' ,'train/GT_CD')
-
-LEVIR_TRAIN_PATH_T1 = os.path.join(SECOND_DATASET_PATH,'LEVIR-CD' ,'train/A')
-LEVIR_TRAIN_PATH_T2 = os.path.join(SECOND_DATASET_PATH,'LEVIR-CD' ,'train/B')
-LEVIR_TRAIN_LABEL_PATH = os.path.join(SECOND_DATASET_PATH,'LEVIR-CD' ,'train/label')
-
-LEVIR_TEST_PATH_T1 = os.path.join(SECOND_DATASET_PATH,'LEVIR-CD' ,'test/A')
-LEVIR_TEST_PATH_T2 = os.path.join(SECOND_DATASET_PATH,'LEVIR-CD' ,'test/B')
-LEVIR_TEST_LABEL_PATH = os.path.join(SECOND_DATASET_PATH,'LEVIR-CD' ,'test/label')
-
-def list_out_SECOND():
-    files_T1 = []
-    for file in os.listdir(TRAIN_PATH_T1):
-        files_T1.append(file)
-    files_T2 = []
-    for file in os.listdir(TRAIN_PATH_T2):
-        files_T2.append(file)
-    files_LABEL = []
-    for file in os.listdir(LABEL_PATH):
-        files_LABEL.append(file)
-
-    T1_OUT = os.path.join(SECOND_DATASET_PATH, 'T1.txt')
-    T2_OUT = os.path.join(SECOND_DATASET_PATH, 'T2.txt')
-    LABEL_OUT = os.path.join(SECOND_DATASET_PATH, 'GT_CD.txt')
-
-    with open(T1_OUT, 'w+') as f:
-        for item in files_T1:
-            f.write("%s\n" % item)
-
-    with open(T2_OUT, 'w+') as f:
-        for item in files_T2:
-            f.write("%s\n" % item)
-
-    with open(LABEL_OUT, 'w+') as f:
-        for item in files_LABEL:
-            f.write("%s\n" % item)
-
-def list_out_LEVIR():
-    folder_dir = os.path.join(SECOND_DATASET_PATH, 'LEVIR-processed')
+def make_dir(dir_name,folder: str):
+    folder_dir = os.path.join(DATASET_PATH, folder)
     if not os.path.exists(folder_dir):
         os.makedirs(folder_dir)
+    train_dir = os.path.join(folder_dir, dir_name)
+    if not os.path.exists(train_dir):
+        os.makedirs(train_dir)
+    T1_dir = os.path.join(train_dir, 'T1')
+    T2_dir = os.path.join(train_dir, 'T2')
+    GT_dir = os.path.join(train_dir, 'GT')
+    if not os.path.exists(T1_dir):
+        os.makedirs(T1_dir)
+    if not os.path.exists(T2_dir):
+        os.makedirs(T2_dir)
+    if not os.path.exists(GT_dir):
+        os.makedirs(GT_dir)
+    return T1_dir, T2_dir, GT_dir, folder_dir
 
-    def make_dir(dir_name):
-        train_dir = os.path.join(folder_dir, dir_name)
-        if not os.path.exists(train_dir):
-            os.makedirs(train_dir)
-        T1_dir = os.path.join(train_dir, 'T1')
-        T2_dir = os.path.join(train_dir, 'T2')
-        GT_dir = os.path.join(train_dir, 'GT')
-        if not os.path.exists(T1_dir):
-            os.makedirs(T1_dir)
-        if not os.path.exists(T2_dir):
-            os.makedirs(T2_dir)
-        if not os.path.exists(GT_dir):
-            os.makedirs(GT_dir)
-        return T1_dir, T2_dir, GT_dir
-
-    T1_dir, T2_dir, GT_dir = make_dir('train')
-    for file in os.listdir(LEVIR_TRAIN_PATH_T1):
-        name = file.split('_')[1]
-        num = name.split('.')[0]
-        name = num.zfill(5) + '.png'
-        shutil.copy(os.path.join(LEVIR_TRAIN_PATH_T1, file), os.path.join(T1_dir, name))
-    for file in os.listdir(LEVIR_TRAIN_PATH_T2):
-        name = file.split('_')[1]
-        num = name.split('.')[0]
-        name = num.zfill(5) + '.png'
-        shutil.copy(os.path.join(LEVIR_TRAIN_PATH_T2, file), os.path.join(T2_dir, name))
-    for file in os.listdir(LEVIR_TRAIN_LABEL_PATH):
-        name = file.split('_')[1]
-        num = name.split('.')[0]
-        name = num.zfill(5) + '.png'
-        shutil.copy(os.path.join(LEVIR_TRAIN_LABEL_PATH, file), os.path.join(GT_dir, name))
-
-    files_train = []
+def make_list_train_test(T1_dir, name, folder_dir):
+    files = []
     for file in os.listdir(T1_dir):
-        files_train.append(file)
-
-    with open (os.path.join(folder_dir, 'train.txt'), 'w+') as f:
-        for item in files_train:
+        files.append(file)
+    with open (os.path.join(folder_dir, name), 'w+') as f:
+        for item in files:
             f.write("%s\n" % item)
 
-    T1_dir, T2_dir, GT_dir = make_dir('test')
-    for file in os.listdir(LEVIR_TEST_PATH_T1):
-        name = file.split('_')[1]
-        num = name.split('.')[0]
-        name = num.zfill(5) + '.png'
-        shutil.copy(os.path.join(LEVIR_TEST_PATH_T1, file), os.path.join(T1_dir, name))
-    for file in os.listdir(LEVIR_TEST_PATH_T2):
-        name = file.split('_')[1]
-        num = name.split('.')[0]
-        name = num.zfill(5) + '.png'
-        shutil.copy(os.path.join(LEVIR_TEST_PATH_T2, file), os.path.join(T2_dir, name))
-    for file in os.listdir(LEVIR_TEST_LABEL_PATH):
-        name = file.split('_')[1]
-        num = name.split('.')[0]
-        name = num.zfill(5) + '.png'
-        shutil.copy(os.path.join(LEVIR_TEST_LABEL_PATH, file), os.path.join(GT_dir, name))
+SECOND_TRAIN_PATH_T1 = os.path.join(DATASET_PATH,'SECOND' ,'train/T1')
+SECOND_TRAIN_PATH_T2 = os.path.join(DATASET_PATH,'SECOND' ,'train/T2')
+SECOND_LABEL_PATH = os.path.join(DATASET_PATH,'SECOND' ,'train/GT_CD')
 
-    files_test = []
-    for file in os.listdir(T1_dir):
-        files_test.append(file)
-    
-    with open (os.path.join(folder_dir, 'test.txt'), 'w+') as f:
-        for item in files_test:
-            f.write("%s\n" % item)
+SECOND_TEST_PATH_T1 = os.path.join(DATASET_PATH,'SECOND' ,'test/T1')
+SECOND_TEST_PATH_T2 = os.path.join(DATASET_PATH,'SECOND' ,'test/T2')
+SECOND_TEST_LABEL_PATH = os.path.join(DATASET_PATH,'SECOND' ,'test/GT_CD')
+
+class SECOND:
+    def __init__(self):
+        print('Processing SECOND dataset...')
+
+    def update_bar(self, total_files, desc, original_path, new_path):
+        with tqdm(total=total_files, desc= desc, unit= 'file') as pbar:
+            for file in os.listdir(original_path):
+                name = file.split('_')[1]
+                num = name.split('.')[0]
+                name = num.zfill(5) + '.png'
+                shutil.copy(os.path.join(original_path, file), os.path.join(new_path, name))
+                pbar.update(1)
+
+    def list_out_SECOND(self):
+        T1_dir, T2_dir, GT_dir, folder_dir = make_dir('train','SECOND-processed')
+        total_files_T1 = len(os.listdir(SECOND_TRAIN_PATH_T1))
+        self.update_bar(total_files_T1, "Processing T1 in train Dataset", SECOND_TRAIN_PATH_T1, T1_dir)
+        self.update_bar(total_files_T1, "Processing T2 in train Dataset", SECOND_TRAIN_PATH_T2, T2_dir)
+        self.update_bar(total_files_T1, "Processing GT in train Dataset", SECOND_LABEL_PATH, GT_dir)
+
+        make_list_train_test(T1_dir, 'train.txt', folder_dir)
+
+        T1_dir, T2_dir, GT_dir, folder_dir = make_dir('test','SECOND-processed')
+        self.update_bar(total_files_T1, "Processing T1 in test Dataset", SECOND_TEST_PATH_T1, T1_dir)
+        self.update_bar(total_files_T1, "Processing T2 in test Dataset", SECOND_TEST_PATH_T2, T2_dir)
+        self.update_bar(total_files_T1, "Processing GT in test Dataset", SECOND_TEST_LABEL_PATH, GT_dir)
+
+        make_list_train_test(T1_dir, 'test.txt', folder_dir)
 
 
-SYSU_TRAIN_PATH_T1 = os.path.join(SECOND_DATASET_PATH,'SYSU' ,'train/train/time1')
-SYSU_TRAIN_PATH_T2 = os.path.join(SECOND_DATASET_PATH,'SYSU' ,'train/train/time2')
-SYSU_TRAIN_LABEL_PATH = os.path.join(SECOND_DATASET_PATH,'SYSU' ,'train/train/label')
+LEVIR_TRAIN_PATH_T1 = os.path.join(DATASET_PATH,'LEVIR-CD' ,'train/A')
+LEVIR_TRAIN_PATH_T2 = os.path.join(DATASET_PATH,'LEVIR-CD' ,'train/B')
+LEVIR_TRAIN_LABEL_PATH = os.path.join(DATASET_PATH,'LEVIR-CD' ,'train/label')
 
-SYSU_TEST_PATH_T1 = os.path.join(SECOND_DATASET_PATH,'SYSU' ,'train/test/time1')
-SYSU_TEST_PATH_T2 = os.path.join(SECOND_DATASET_PATH,'SYSU' ,'train/test/time2')
-SYSU_TEST_LABEL_PATH = os.path.join(SECOND_DATASET_PATH,'SYSU' ,'train/test/label')
+LEVIR_TEST_PATH_T1 = os.path.join(DATASET_PATH,'LEVIR-CD' ,'test/A')
+LEVIR_TEST_PATH_T2 = os.path.join(DATASET_PATH,'LEVIR-CD' ,'test/B')
+LEVIR_TEST_LABEL_PATH = os.path.join(DATASET_PATH,'LEVIR-CD' ,'test/label')
 
-def list_out_SYSU():
-    folder_dir = os.path.join(SECOND_DATASET_PATH, 'SYSU-processed')
-    if not os.path.exists(folder_dir):
-        os.makedirs(folder_dir)
+class LEVIR_CD:
+    def __init__(self):
+        print('Processing LEVIR-CD dataset...')
 
-    def make_dir(dir_name):
-        train_dir = os.path.join(folder_dir, dir_name)
-        if not os.path.exists(train_dir):
-            os.makedirs(train_dir)
-        T1_dir = os.path.join(train_dir, 'T1')
-        T2_dir = os.path.join(train_dir, 'T2')
-        GT_dir = os.path.join(train_dir, 'GT')
-        if not os.path.exists(T1_dir):
-            os.makedirs(T1_dir)
-        if not os.path.exists(T2_dir):
-            os.makedirs(T2_dir)
-        if not os.path.exists(GT_dir):
-            os.makedirs(GT_dir)
-        return T1_dir, T2_dir, GT_dir
+    def update_bar(self, total_files, desc, original_path, new_path):
+        with tqdm(total=total_files, desc= desc, unit= 'file') as pbar:
+            for file in os.listdir(original_path):
+                name = file.split('_')[1]
+                num = name.split('.')[0]
+                name = num.zfill(5) + '.png'
+                shutil.copy(os.path.join(original_path, file), os.path.join(new_path, name))
+                pbar.update(1)
 
-    T1_dir, T2_dir, GT_dir = make_dir('train')
-    for file in os.listdir(SYSU_TRAIN_PATH_T1):
-        shutil.copy(os.path.join(SYSU_TRAIN_PATH_T1, file), os.path.join(T1_dir, file))
-    for file in os.listdir(SYSU_TRAIN_PATH_T2):
-        shutil.copy(os.path.join(SYSU_TRAIN_PATH_T2, file), os.path.join(T2_dir, file))
-    for file in os.listdir(SYSU_TRAIN_LABEL_PATH):
-        shutil.copy(os.path.join(SYSU_TRAIN_LABEL_PATH, file), os.path.join(GT_dir, file))
+    def list_out_LEVIR(self):
+        T1_dir, T2_dir, GT_dir, folder_dir = make_dir('train','LEVIR-processed')
+        total_files_T1 = len(os.listdir(LEVIR_TRAIN_PATH_T1))
+        self.update_bar(total_files_T1, "Processing T1 in train Dataset", LEVIR_TRAIN_PATH_T1, T1_dir)
+        self.update_bar(total_files_T1, "Processing T2 in train Dataset", LEVIR_TRAIN_PATH_T2, T2_dir)
+        self.update_bar(total_files_T1, "Processing GT in train Dataset", LEVIR_TRAIN_LABEL_PATH, GT_dir)
 
-    files_train = []
-    for file in os.listdir(T1_dir):
-        files_train.append(file)
-    
-    with open (os.path.join(folder_dir, 'train.txt'), 'w+') as f:
-        for item in files_train:
-            f.write("%s\n" % item)
+        make_list_train_test(T1_dir, 'train.txt', folder_dir)
 
-    T1_dir, T2_dir, GT_dir = make_dir('test')
-    for file in os.listdir(SYSU_TEST_PATH_T1):
-        shutil.copy(os.path.join(SYSU_TEST_PATH_T1, file), os.path.join(T1_dir, file))
-    for file in os.listdir(SYSU_TEST_PATH_T2):
-        shutil.copy(os.path.join(SYSU_TEST_PATH_T2, file), os.path.join(T2_dir, file))
-    for file in os.listdir(SYSU_TEST_LABEL_PATH):
-        shutil.copy(os.path.join(SYSU_TEST_LABEL_PATH, file), os.path.join(GT_dir, file))
+        T1_dir, T2_dir, GT_dir, folder_dir = make_dir('test','LEVIR-processed')
+        self.update_bar(total_files_T1, "Processing T1 in test Dataset", LEVIR_TEST_PATH_T1, T1_dir)
+        self.update_bar(total_files_T1, "Processing T2 in test Dataset", LEVIR_TEST_PATH_T2, T2_dir)
+        self.update_bar(total_files_T1, "Processing GT in test Dataset", LEVIR_TEST_LABEL_PATH, GT_dir)
 
-    files_test = []
-    for file in os.listdir(T1_dir):
-        files_test.append(file)
-    
-    with open (os.path.join(folder_dir, 'test.txt'), 'w+') as f:
-        for item in files_test:
-            f.write("%s\n" % item)
+        make_list_train_test(T1_dir, 'test.txt', folder_dir)
+
+
+SYSU_TRAIN_PATH_T1 = os.path.join(DATASET_PATH,'SYSU' ,'train/train/time1')
+SYSU_TRAIN_PATH_T2 = os.path.join(DATASET_PATH,'SYSU' ,'train/train/time2')
+SYSU_TRAIN_LABEL_PATH = os.path.join(DATASET_PATH,'SYSU' ,'train/train/label')
+
+SYSU_TEST_PATH_T1 = os.path.join(DATASET_PATH,'SYSU' ,'train/test/time1')
+SYSU_TEST_PATH_T2 = os.path.join(DATASET_PATH,'SYSU' ,'train/test/time2')
+SYSU_TEST_LABEL_PATH = os.path.join(DATASET_PATH,'SYSU' ,'train/test/label')
+
+class SYSU_CD:
+    def __init__(self):
+        print('Processing SYSU-CD dataset...')
+
+    def update_bar(self, total_files, desc, original_path, new_path):
+        with tqdm(total=total_files, desc= desc, unit= 'file') as pbar:
+            for file in os.listdir(original_path):
+                name = file.split('_')[1]
+                num = name.split('.')[0]
+                name = num.zfill(5) + '.png'
+                shutil.copy(os.path.join(original_path, file), os.path.join(new_path, name))
+                pbar.update(1)
+
+    def list_out_SYSU(self):
+        T1_dir, T2_dir, GT_dir, folder_dir = make_dir('train','SYSU-processed')
+        total_files_T1 = len(os.listdir(SYSU_TRAIN_PATH_T1))
+        self.update_bar(total_files_T1, "Processing T1 in train Dataset", SYSU_TRAIN_PATH_T1, T1_dir)
+        self.update_bar(total_files_T1, "Processing T2 in train Dataset", SYSU_TRAIN_PATH_T2, T2_dir)
+        self.update_bar(total_files_T1, "Processing GT in train Dataset", SYSU_TRAIN_LABEL_PATH, GT_dir)
+
+        make_list_train_test(T1_dir, 'train.txt', folder_dir)
+
+        T1_dir, T2_dir, GT_dir, folder_dir = make_dir('test','SYSU-processed')
+        self.update_bar(total_files_T1, "Processing T1 in test Dataset", SYSU_TEST_PATH_T1, T1_dir)
+        self.update_bar(total_files_T1, "Processing T2 in test Dataset", SYSU_TEST_PATH_T2, T2_dir)
+        self.update_bar(total_files_T1, "Processing GT in test Dataset", SYSU_TEST_LABEL_PATH, GT_dir)
+
+        make_list_train_test(T1_dir, 'test.txt', folder_dir)
 
 if __name__ == '__main__':
-    list_out_SYSU()
+    print("Select Dataset")
+    print("1. SECOND")
+    print("2. LEVIR-CD")
+    print("3. SYSU-CD")
+    choice = int(input())
+    if choice == 1:
+        SECOND().list_out_SECOND()
+    elif choice == 2:
+        LEVIR_CD().list_out_LEVIR()
+    elif choice == 3:
+        SYSU_CD().list_out_SYSU()
+    else:
+        print("Invalid Choice")
