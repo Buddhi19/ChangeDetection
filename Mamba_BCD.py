d1c34372352dece58e32080539215068aa6ee6a7
Buddhi19 <wbuddhi@icloud.com>
2025-02-01 14:12:34 +0530

    Add SYSU dataset paths and update training functions; create output file for configuration

--- Mamba_BCD.py
+++ Mamba_BCD.py
@@ -20,6 +20,12 @@ LEVIR_test_dataset_path = os.path.join(LEVIR_dataset_path, 'test')
 LEVIR_train_data_list_path = os.path.join(LEVIR_dataset_path, 'train.txt')
 LEVIR_test_data_list_path = os.path.join(LEVIR_dataset_path, 'test.txt')
 
+SYSU_dataset_path = os.path.join(main_dir, 'Datasets', 'SYSU-processed')
+SYSU_train_dataset_path = os.path.join(SYSU_dataset_path, 'train')
+SYSU_test_dataset_path = os.path.join(SYSU_dataset_path, 'test')
+SYSU_train_data_list_path = os.path.join(SYSU_dataset_path, 'train.txt')
+SYSU_test_data_list_path = os.path.join(SYSU_dataset_path, 'test.txt')
+
 train_data_list = []
 with open(LEVIR_train_data_list_path, 'r') as f:
     for line in f:
@@ -39,11 +45,11 @@ class ARGS:
         self.type = 'train'
         self.shuffle = True
         self.crop_size = 256
-        self.batch_size = 5
+        self.batch_size = 10
         self.max_iters = 320000
         self.start_iter = 0
         self.cuda = True
-        self.model_type = 'MambaBCD_Small'
+        self.model_type = 'MambaBCD_Base'
         self.learning_rate = 1e-4
         self.momentum = 0.9
         self.weight_decay = 5e-4
@@ -55,8 +61,29 @@ class ARGS:
         self.model_param_path = model_path
         self.resume = None
 
-args = ARGS()
+def LEVIR_main():
+    args = ARGS()
+    trainer_LEVIR = train_MambaBCD.Trainer(args)
+    trainer_LEVIR.training()
+    trainer_LEVIR.validation()
 
-trainer_LEVIR = train_MambaBCD.Trainer(args)
-trainer_LEVIR.train()
-trainer_LEVIR.validation()
+def SYSU_main():
+    args = ARGS()
+    args.dataset_path = SYSU_dataset_path
+    args.train_dataset_path = SYSU_train_dataset_path
+    args.train_data_name_list = []
+    with open(SYSU_train_data_list_path, 'r') as f:
+        for line in f:
+            args.train_data_name_list.append(line.strip())
+    args.test_dataset_path = SYSU_test_dataset_path
+    args.test_data_name_list = []
+    with open(SYSU_test_data_list_path, 'r') as f:
+        for line in f:
+            args.test_data_name_list.append(line.strip())
+    trainer_SYSU = train_MambaBCD.Trainer(args)
+    trainer_SYSU.training()
+    trainer_SYSU.validation()
+
+if __name__ == "__main__":
+    SYSU_main()
+
